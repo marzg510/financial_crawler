0f6b7a8723cf5775ed86c0304c44987a289dbfa9
Masaru Gotou <masaru.gotou@gmail.com>
2025-05-29 22:20:04 +0900

    Playwrightで、Yahooのトップページを取ってきて、タイトルを返却する Fixes #30

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Python image to use.
-FROM python:3.12-alpine
+FROM mcr.microsoft.com/playwright/python:v1.52.0-jammy
 
 # Set the working directory to /app
 WORKDIR /app
@@ -9,6 +9,9 @@ COPY requirements.txt .
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Playwrightのブラウザバイナリをインストール
+# RUN playwright install --with-deps
 
 # Copy the rest of the working directory contents into the container at /app
 COPY . .
--- app.py
+++ app.py
@@ -2,26 +2,37 @@
 A sample Hello World server.
 """
 import os
-
-from flask import Flask, render_template
+from playwright.sync_api import sync_playwright
+from flask import Flask, jsonify
 
 # pylint: disable=C0103
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    message = "It's running!"
+def exec_playwright():
+  # playwrightの実装  
+  try:    
+    with sync_playwright() as p:      
+      # Chromium (Web Browser)のインスタンスを作成する      
+      browser = p.chromium.launch(headless=True)        
+      
+      # 新しいページを作成する      
+      page = browser.new_page()        
+      print('page: ', page)     
+      
+      # page.goto() で Yahoo のサイトにアクセス       
+      page.goto('https://www.yahoo.co.jp/')              
 
-    """Get Cloud Run environment variables."""
-    service = os.environ.get('K_SERVICE', 'Unknown service')
-    revision = os.environ.get('K_REVISION', 'Unknown revision')
+      # title tag の値（ページタイトル）を取得し辞書に格納     
+      title = page.title()      
+ 
+      # Browser を閉じる      
+      browser.close()
 
-    return render_template('index.html',
-        message=message,
-        Service=service,
-        Revision=revision)
+      return jsonify({'title': title}), 200
+  except Exception as e:    
+    return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
